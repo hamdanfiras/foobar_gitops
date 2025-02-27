e1351aa2b2919e359d2d96acc9003806db5596d2
Firas Hamdan <hamdanfiras@gmail.com>
2025-02-27 19:51:46 +0300

    changes2

--- gcloud.sh
+++ gcloud.sh
@@ -4,11 +4,13 @@ gcloud container clusters create foobar --zone us-east1-b
 gcloud container clusters get-credentials foobar
 
 kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 exit 0
 
 # after
 
-kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+kubectl port-forward svc/argocd-server -n argocd 8086:443
 kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d && echo
 
+gcloud container clusters describe foobar --zone us-east1-b --format="value(endpoint)"
