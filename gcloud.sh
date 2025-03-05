cb1ebb6f7cc88c3fc6cc1f05f6eeaf8888898851
Firas <fhamdan@saib.com.sa>
2025-03-05 14:15:24 +0300

    chnages

--- gcloud.sh
+++ gcloud.sh
@@ -1,7 +1,7 @@
-gcloud config set project dummy-59e81
-gcloud config set compute/zone us-east1-b
-gcloud container clusters create foobar --zone us-east1-b
-gcloud container clusters get-credentials foobar
+gcloud config set project macro-shadow-452810-p9
+gcloud config set compute/zone us-central1 
+gcloud container clusters create autopilot-cluster-1 --zone us-central1 
+gcloud container clusters get-credentials autopilot-cluster-1
 
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
@@ -13,4 +13,4 @@ exit 0
 kubectl port-forward svc/argocd-server -n argocd 8086:443
 kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d && echo
 
-gcloud container clusters describe foobar --zone us-east1-b --format="value(endpoint)"
+gcloud container clusters describe foobar --zone us-central1  --format="value(endpoint)"
