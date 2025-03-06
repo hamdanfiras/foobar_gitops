76a1882365e09c6f767faa97e0476a7c8446c9fb
Firas <fhamdan@saib.com.sa>
2025-03-06 12:57:31 +0300

    destination

--- gcloud.sh
+++ gcloud.sh
@@ -1,12 +1,41 @@
 #!/bin/bash
 
+project="macro-shadow-452810-p9"
 cluster="foobar"
 region="us-central1"
+zone="us-central1-a"
 
-gcloud config set project macro-shadow-452810-p9
+gcloud config set project $project
 gcloud config set compute/zone $region
-gcloud beta container --project "macro-shadow-452810-p9" clusters create-auto "$cluster" --region "us-central1" --release-channel "regular" --tier "standard" --enable-ip-access --no-enable-google-cloud-access --network "projects/macro-shadow-452810-p9/global/networks/default" --subnetwork "projects/macro-shadow-452810-p9/regions/us-central1/subnetworks/default" --cluster-ipv4-cidr "/17" --binauthz-evaluation-mode=DISABLED
-gcloud container clusters create $cluster --zone $region
+# gcloud beta container --project "$project" clusters create-auto "$cluster" --region "us-central1" --release-channel "regular" --tier "standard" --enable-ip-access --no-enable-google-cloud-access --network "projects/$project/global/networks/default" --subnetwork "projects/$project/regions/us-central1/subnetworks/default" --cluster-ipv4-cidr "/17" --binauthz-evaluation-mode=DISABLED
+# gcloud container clusters create "$cluster" \
+#   --project "$project" \
+#   --region $region \
+#   --release-channel "regular" \
+#   --tier "standard" \
+#   --enable-ip-access \
+#   --no-enable-google-cloud-access \
+#   --network "projects/$project/global/networks/default" \
+#   --subnetwork "projects/$project/regions/us-central1/subnetworks/default" \
+#   --cluster-ipv4-cidr "/17" \
+#   --disk-size "100" \
+#   --binauthz-evaluation-mode=DISABLED
+
+gcloud container clusters create $cluster \
+  --project "$project" \
+  --zone $zone \
+  --release-channel "regular" \
+  --tier "standard" \
+  --enable-ip-access \
+  --no-enable-google-cloud-access \
+  --network "projects/$project/global/networks/default" \
+  --subnetwork "projects/$project/regions/us-central1/subnetworks/default" \
+  --cluster-ipv4-cidr "/17" \
+  --disk-size "100" \
+  --binauthz-evaluation-mode=DISABLED
+
+
+# gcloud container clusters create $cluster --zone $region
 gcloud container clusters get-credentials $cluster
 
 kubectl create namespace argocd
