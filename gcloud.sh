45928a59f69c00046e049ac21dfbe261d2489f6f
Firas <fhamdan@saib.com.sa>
2025-03-10 09:55:24 +0300

    changes

--- gcloud.sh
+++ gcloud.sh
@@ -8,20 +8,6 @@ zone="us-central1-a"
 gcloud config set project $project
 gcloud config set compute/zone $region
 
-# gcloud beta container --project "$project" clusters create-auto "$cluster" --region "us-central1" --release-channel "regular" --tier "standard" --enable-ip-access --no-enable-google-cloud-access --network "projects/$project/global/networks/default" --subnetwork "projects/$project/regions/us-central1/subnetworks/default" --cluster-ipv4-cidr "/17" --binauthz-evaluation-mode=DISABLED
-# gcloud container clusters create "$cluster" \
-#   --project "$project" \
-#   --region $region \
-#   --release-channel "regular" \
-#   --tier "standard" \
-#   --enable-ip-access \
-#   --no-enable-google-cloud-access \
-#   --network "projects/$project/global/networks/default" \
-#   --subnetwork "projects/$project/regions/us-central1/subnetworks/default" \
-#   --cluster-ipv4-cidr "/17" \
-#   --disk-size "100" \
-#   --binauthz-evaluation-mode=DISABLED
-
 gcloud container clusters create $cluster \
   --project "$project" \
   --zone $zone \
@@ -36,17 +22,18 @@ gcloud container clusters create $cluster \
   --binauthz-evaluation-mode=DISABLED
 
 
-# gcloud container clusters create $cluster --zone $region
 gcloud container clusters get-credentials $cluster
 
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
+sleep 2
+
+
+password=$(kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d) && echo "ArgoCD initial admin password: $password"
 
 kubectl port-forward svc/argocd-server -n argocd 8086:443 &> /dev/null &
 
-password=$(kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d)
-echo "ArgoCD initial admin password: $password"
 
 sleep 2
 
@@ -61,18 +48,12 @@ exit 0
 
 # after
 
-# kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d && echo
-
-
 kubectl port-forward svc/argocd-server -n argocd 8086:443 &
 
 password=$(kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d)
 echo "ArgoCD initial admin password: $password" && argocd login {argocd_server} --username admin --password @password
 
-
-
 kubectl port-forward svc/argocd-server -n argocd 8086:443
-
 
 gcloud container clusters describe foobar --zone $region --format="value(endpoint)"
 
