6b4a9540a011051d3bfc97156643e5165a4245ae
Firas <fhamdan@saib.com.sa>
2025-03-16 13:30:10 +0300

    mount fix

--- aftergcloud.sh
+++ aftergcloud.sh
@@ -1,7 +1,7 @@
+password=$(kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d) && echo "ArgoCD initial admin password: $password"
+sleep 1
 kubectl port-forward svc/argocd-server -n argocd 8086:443 &> /dev/null &
 
-password=$(kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d)
-echo "ArgoCD initial admin password: $password"
 
 sleep 2
 
@@ -9,4 +9,4 @@ argocd login "localhost:8086" --username admin --password $password --insecure
 
 argocd repo add https://github.com/hamdanfiras/foobar_gitops --username hamdanfiras --password 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
 
-kubectl apply -f ./apps/root-dev.yml
+kubectl apply -f ./apps/root-dev.yml  
