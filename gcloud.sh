82528c502319d74f256b5f74af8f19cbb0d30eae
Firas <fhamdan@saib.com.sa>
2025-03-17 11:37:10 +0300

    remove monitoring

--- gcloud.sh
+++ gcloud.sh
@@ -27,6 +27,9 @@ gcloud container clusters get-credentials $cluster
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
+kubectl create -f https://download.elastic.co/downloads/eck/2.16.1/crds.yaml
+kubectl apply -f https://download.elastic.co/downloads/eck/2.16.1/operator.yaml
+
 exit 0
 
 
